06dabce88c18db6f4249f12bc4034b57cfed088b
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2023-11-20 16:42:41 +0900

    授業

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        rb.velocity = new Vector3(1.0f * Input.GetAxis("leftStickX"), rb.velocity.y, -1.0f * Input.GetAxis("leftStickY"));
+
     }
 }
