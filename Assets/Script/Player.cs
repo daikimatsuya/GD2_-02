ba2cd8f0a210c48081046d8f865e6ea3d632df5b
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2023-11-24 17:11:58 +0900

    授業

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,11 +10,12 @@ public class Player : MonoBehaviour
 
     Transform llTransform;
     Transform rlTransform;
-
+    new Transform transform;
 
 
     private float leftRad;
     private float rightRad;
+    private float playerDegree;
 
     // Start is called before the first frame update
     void Start()
@@ -25,17 +26,12 @@ public class Player : MonoBehaviour
 
         llTransform=leftLazer.GetComponent<Transform>();
         rlTransform=rightLazer.GetComponent<Transform>();
+        transform = GetComponent<Transform>();
+
+        playerDegree = transform.localRotation.eulerAngles.z;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Move(float leftDegree,float rightDegree)
     {
-        llTransform.localScale = Vector3.zero;
-        rlTransform.localScale= Vector3.zero;
-        // rb.velocity = new Vector3(2.0f * (Input.GetAxis("leftStickX")+Input.GetAxis("rightStickX")), rb.velocity.y, -2.0f * (Input.GetAxis("leftStickY")+Input.GetAxis("rightStickY")));
-        float leftDegree = Mathf.Atan2(Input.GetAxis("leftStickX"), Input.GetAxis("leftStickY")) * Mathf.Rad2Deg;
-        float rightDegree = Mathf.Atan2(Input.GetAxis("rightStickX"), Input.GetAxis("rightStickY")) * Mathf.Rad2Deg;
-
         if (Input.GetAxis("leftStickX") != 0 || Input.GetAxis("leftStickY") != 0)
         {
             llTransform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
@@ -68,7 +64,20 @@ public class Player : MonoBehaviour
                 rightDegree = 105;
             }
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        llTransform.localScale = Vector3.zero;
+        rlTransform.localScale= Vector3.zero;
+      
+        float leftDegree = Mathf.Atan2(Input.GetAxis("leftStickX"), Input.GetAxis("leftStickY")) * Mathf.Rad2Deg;
+        float rightDegree = Mathf.Atan2(Input.GetAxis("rightStickX"), Input.GetAxis("rightStickY")) * Mathf.Rad2Deg;
+        Move(leftDegree,rightDegree);
+       
         llTransform.localRotation= Quaternion.Euler(90,0, leftDegree+180);
         rlTransform.localRotation = Quaternion.Euler(90, 0, rightDegree+180);
+        rb.velocity = new Vector3(2.0f * (Input.GetAxis("leftStickX") + Input.GetAxis("rightStickX")), rb.velocity.y, -2.0f * (Input.GetAxis("leftStickY") + Input.GetAxis("rightStickY")));
     }
 }
