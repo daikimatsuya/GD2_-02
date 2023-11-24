8efb581c7309f468a8eccda23b2b3d127ff96a0b
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2023-11-24 14:46:52 +0900

    角度取得

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody rb;
+    GameObject leftLazer;
+    GameObject rightLazer;
+
+    Transform llTransform;
+    Transform rlTransform;
+
+    private float leftRad;
+    private float rightRad;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        leftLazer = transform.Find("LeftLazer").gameObject;
+        rightLazer = transform.Find("RightLazer").gameObject;
+
+        llTransform=leftLazer.GetComponent<Transform>();
+        rlTransform=rightLazer.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        rb.velocity = new Vector3(1.0f * Input.GetAxis("leftStickX"), rb.velocity.y, -1.0f * Input.GetAxis("leftStickY"));
+        // rb.velocity = new Vector3(2.0f * (Input.GetAxis("leftStickX")+Input.GetAxis("rightStickX")), rb.velocity.y, -2.0f * (Input.GetAxis("leftStickY")+Input.GetAxis("rightStickY")));
+        float leftDegree = Mathf.Atan2(Input.GetAxis("leftStickX"), Input.GetAxis("leftStickY")) * Mathf.Rad2Deg;
+        float rightDegree = Mathf.Atan2(Input.GetAxis("rightStickX"), Input.GetAxis("rightStickY")) * Mathf.Rad2Deg;
 
+        if (leftDegree < 0)
+        {
+            leftDegree += 360;
+        }
+        if (rightDegree < 0)
+        {
+            rightDegree += 360;
+        }
+
+        llTransform.localRotation= Quaternion.Euler(90,0, leftDegree);
+        rlTransform.localRotation = Quaternion.Euler(90, 0, rightDegree);
     }
 }
