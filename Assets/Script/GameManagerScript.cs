da356a38f88e2e09a5655a786ed8b1ddfb2f6522
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2023-12-08 15:13:01 +0900

    ウィンドウモード

--- Assets/Script/GameManagerScript.cs
+++ Assets/Script/GameManagerScript.cs
@@ -8,6 +8,7 @@ public class GameManagerScript : MonoBehaviour
     void Start()
     {
         Application.targetFrameRate = 60;
+        Screen.SetResolution(1080, 1280, false);
     }
 
     // Update is called once per frame
